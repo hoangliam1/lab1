d5bdae5742a18fa055a6e6b4de4d297f1002f9f7
hoangliamm <hoanglam2108005@gmail.com>
2025-04-24 14:10:59 +0700

    fix and simplify parseXDice logic

---
+++ src/project3/DiceParser.java
@@ -0,0 +1,220 @@
+package project3;
+
+import java.util.*;
+
+/**
+ * @author Hoang Lam.
+ */
+public class DiceParser {
+
+    /* this is a helper class to manage the input "stream" */
+    private static class StringStream {
+
+        private StringBuffer buff;
+
+        public StringStream(String s) {
+            buff = new StringBuffer(s);
+        }
+
+        private void munchWhiteSpace() {
+            int index = 0;
+            char curr;
+            while (index < buff.length()) {
+                curr = buff.charAt(index);
+                if (!Character.isWhitespace(curr)) {
+                    break;
+                }
+                index++;
+            }
+            buff = buff.delete(0, index);
+        }
+
+        public boolean isEmpty() {
+            munchWhiteSpace();
+            return buff.toString().equals("");
+        }
+
+        public Integer getInt() {
+            return readInt();
+        }
+
+        public Integer readInt() {
+            int index = 0;
+            char curr;
+            munchWhiteSpace();
+            while (index < buff.length()) {
+                curr = buff.charAt(index);
+                if (!Character.isDigit(curr)) {
+                    break;
+                }
+                index++;
+            }
+            try {
+                Integer ans;
+                ans = Integer.parseInt(buff.substring(0, index));
+                buff = buff.delete(0, index);
+                return ans;
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        public Integer readSgnInt() {
+            munchWhiteSpace();
+            StringStream state = save();
+            if (checkAndEat("+")) {
+                Integer ans = readInt();
+                if (ans != null) {
+                    return ans;
+                }
+                restore(state);
+                return null;
+            }
+            if (checkAndEat("-")) {
+                Integer ans = readInt();
+                if (ans != null) {
+                    return -ans;
+                }
+                restore(state);
+                return null;
+            }
+            return readInt();
+        }
+
+        public boolean checkAndEat(String s) {
+            munchWhiteSpace();
+            if (buff.indexOf(s) == 0) {
+                buff = buff.delete(0, s.length());
+                return true;
+            }
+            return false;
+        }
+
+        public StringStream save() {
+            return new StringStream(buff.toString());
+        }
+
+        public void restore(StringStream ss) {
+            this.buff = new StringBuffer(ss.buff.toString());
+        }
+
+        public String toString() {
+            return buff.toString();
+        }
+    }
+
+    public static Vector<DieRoll> parseRoll(String s) {
+        StringStream ss = new StringStream(s.toLowerCase());
+        Vector<DieRoll> v = parseRollInner(ss, new Vector<DieRoll>());
+        if (ss.isEmpty()) {
+            return v;
+        }
+        return null;
+    }
+
+    private static Vector<DieRoll> parseRollInner(StringStream ss, Vector<DieRoll> v) {
+        Vector<DieRoll> r = parseXDice(ss);
+        if (r == null) {
+            return null;
+        }
+        v.addAll(r);
+        if (ss.checkAndEat(";")) {
+            return parseRollInner(ss, v);
+        }
+        return v;
+    }
+
+    private static Vector<DieRoll> parseXDice(StringStream ss) {
+        StringStream saved = ss.save();
+        // Refactored: Fix lỗi cú pháp, đơn giản hoá logic đọc số lần nhân
+        Integer x = ss.getInt();
+        int num = 1;
+        if (x != null) {
+            if (ss.checkAndEat("x")) {
+                num = x;
+            } else {
+                ss.restore(saved); // khôi phục nếu không có 'x'
+            }
+        }
+        DieRoll dr = parseDice(ss);
+        if (dr == null) {
+            return null;
+        }
+        Vector<DieRoll> ans = new Vector<DieRoll>();
+        for (int i = 0; i < num; i++) {
+            ans.add(dr);
+        }
+        return ans;
+    }
+
+    private static DieRoll parseDice(StringStream ss) {
+        return parseDTail(parseDiceInner(ss), ss);
+    }
+
+    private static DieRoll parseDiceInner(StringStream ss) {
+        Integer num = ss.getInt();
+        int dsides;
+        int ndice;
+        if (num == null) {
+            ndice = 1;
+        } else {
+            ndice = num;
+        }
+        if (ss.checkAndEat("d")) {
+            num = ss.getInt();
+            if (num == null) {
+                return null;
+            }
+            dsides = num;
+        } else {
+            return null;
+        }
+        num = ss.readSgnInt();
+        int bonus;
+        if (num == null) {
+            bonus = 0;
+        } else {
+            bonus = num;
+        }
+        return new DieRoll(ndice, dsides, bonus);
+    }
+
+    private static DieRoll parseDTail(DieRoll r1, StringStream ss) {
+        if (r1 == null) {
+            return null;
+        }
+        if (ss.checkAndEat("&")) {
+            DieRoll d2 = parseDice(ss);
+            return parseDTail(new DiceSum(r1, d2), ss);
+        } else {
+            return r1;
+        }
+    }
+
+    private static void test(String s) {
+        Vector<DieRoll> v = parseRoll(s);
+        if (v == null) {
+            System.out.println("Failure:" + s);
+        } else {
+            System.out.println("Results for " + s + ":");
+            for (DieRoll dr : v) {
+                System.out.print(dr);
+                System.out.print(": ");
+                System.out.println(dr.makeRoll());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        test("d6");
+        test("2d6");
+        test("d6+5");
+        test("4X3d8-5");
+        test("12d10+5 & 4d6+2");
+        test("d6 ; 2d4+3");
+        test("4d6+3 ; 8d12 -15 ; 9d10 & 3d6 & 4d12 +17");
+        test("4d6 + xyzzy");
+        test("hi");
+        test("4d4d4");
+    }
+}
